c94dedb79ed5a01ca067bad1f4fa943220b8e3b5
sergeevasveta022-maker <sergeevasveta022@gmail.com>
2026-06-19 23:49:31 -0700

    update

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,22 +5,29 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/my_bd?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
     private static final SessionFactory sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
 
     public static Connection getConnection() {
+        Properties properties = new Properties();
         try {
+            InputStream inputStream = Util.class.getClassLoader().getResourceAsStream("application.properties");
+            properties.load(inputStream);
+            String URL = properties.getProperty("db.url");
+            String USER = properties.getProperty("db.user");
+            String PASSWORD = properties.getProperty("db.password");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
             throw new RuntimeException("Не подключилось", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
     public static SessionFactory getSessionFactory(){
